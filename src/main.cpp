d4e0d8e73837d017cd4a16bf597cb80cade33d12
Zac Scott <zac.scott11@gmail.com>
2021-05-27 05:36:20 +1200

    Fix minor bugs for submission

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  *
  *       Zac Scott (github.com/scottzach1)
  *
- * M5StackTemperature - BLE Server for Temperature Sensor
+ * M5StackHumidity - BLE Server for Humidity Sensor
  */
 #include <BLE2902.h>
 #include <BLEDevice.h>
@@ -25,7 +25,6 @@ static BLEUUID serviceUUID = BLEUUID("4bf524fc-e77c-4b80-bbc6-1345b5f41d76");
 static BLEUUID humiCharacteristicUUID = ((uint16_t)0x2A6F);
 
 BLEDescriptor humiDescriptor(BLEUUID((uint16_t)0x2901));
-
 BLECharacteristic humiCharacteristic(humiCharacteristicUUID, BLECharacteristic::PROPERTY_READ);
 
 BLEServer *pServer = NULL;
@@ -36,8 +35,8 @@ bool deviceConnected = false;
 /**
  * Duty Cycling Timeouts
  */
-const int DUTY_CYCLE_AWAKE = 3;  // seconds awake
-const int DUTY_CYCLE_SLEEP = 4;  // seconds asleep
+const int DUTY_CYCLE_AWAKE = 2;  // seconds awake
+const int DUTY_CYCLE_SLEEP = 2;  // seconds asleep
 const int ACTIVITY_TIMEOUT = 8;  // seconds after BLE activity
 
 /**
@@ -108,9 +107,10 @@ void setup() {
     Serial.begin(115200);
     M5.begin();
     M5.Power.begin();
-    M5.Lcd.clear(BLACK);
-    M5.Lcd.setTextColor(WHITE);
-    M5.Lcd.setBrightness(75);
+    if (DEBUG) {
+        M5.Lcd.clear();
+        M5.Lcd.setBrightness(75);
+    }
     DEBUG_MSG_LN(1, "Humidity node starting...");
 
     // Create BLE server with callbacks.
@@ -120,7 +120,7 @@ void setup() {
     pService = pServer->createService(serviceUUID);
 
     // Add descriptor values.
-    humiDescriptor.setValue("Humitidy: [0,100]%");
+    humiDescriptor.setValue("Humidity: [0,100]%");
 
     // Update characteristics with descriptors.
     humiCharacteristic.addDescriptor(&humiDescriptor);
@@ -167,6 +167,7 @@ void loop() {
     M5.update();
 
     // Handle button presses.
+    if (M5.BtnA.wasReleasefor(5)) clearDisplay();
     if (M5.BtnB.wasReleasefor(5)) toggleDutyCycle();
     if (M5.BtnC.wasReleasefor(5)) M5.Power.reset();
 
